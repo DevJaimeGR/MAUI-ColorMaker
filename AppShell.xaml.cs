4fd4ecb1893364e900c9755014d831a541b457f9
DevJaimeGR <dev.backend.jigr@hotmail.com>
2024-12-18 12:21:08 -0500

    Add color maker project

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUI.Project.ColorMaker
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
